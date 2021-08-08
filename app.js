5e52c8ddd9cb883f050818419e2e9f21c5a4d3d7
Autour Love Jr <autourlovejr@yahoo.com>
2021-08-08 00:32:23 -0400

    hover properties

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-function qual() {
-    var x = document.getElementById("qualifications");
-    if(x.style.display === "none") {
-        x.style.display ="block";
-    } else {
-        x.style.display ="none";
-    }    
-}
+// function qual() {
+//     var x = document.getElementById("qualifications");
+//     if(x.style.display === "none") {
+//         x.style.display ="block";
+//     } else {
+//         x.style.display ="none";
+//     }    
+// }
 
-function car() {
-    var x = document.getElementById("career");
-    if(x.style.display === "none") {
-        x.style.display ="block";
-    } else {
-        x.style.display ="none";
-    }    
-}
+// function car() {
+//     var x = document.getElementById("career");
+//     if(x.style.display === "none") {
+//         x.style.display ="block";
+//     } else {
+//         x.style.display ="none";
+//     }    
+// }
 
-function skill() {
-    var x = document.getElementById("skills");
-    if(x.style.display === "none") {
-        x.style.display ="block";
-    } else {
-        x.style.display ="none";
-    }    
-}
+// function skill() {
+//     var x = document.getElementById("skills");
+//     if(x.style.display === "none") {
+//         x.style.display ="block";
+//     } else {
+//         x.style.display ="none";
+//     }    
+// }
 
-function work() {
-    var x = document.getElementById("work");
-    if(x.style.display === "none") {
-        x.style.display ="block";
-    } else {
-        x.style.display ="none";
-    }    
-}
+// function work() {
+//     var x = document.getElementById("work");
+//     if(x.style.display === "none") {
+//         x.style.display ="block";
+//     } else {
+//         x.style.display ="none";
+//     }    
+// }
 
-function edu() {
-    var x = document.getElementById("education");
-    if(x.style.display === "none") {
-        x.style.display ="block";
-    } else {
-        x.style.display ="none";
-    }    
-}
+// function edu() {
+//     var x = document.getElementById("education");
+//     if(x.style.display === "none") {
+//         x.style.display ="block";
+//     } else {
+//         x.style.display ="none";
+//     }    
+// }
